84a4abc58c4c60c0c15cd53be8803b2d99129683
yupitel <yupitel@yupitelnoMac-mini.local>
2017-10-30 13:49:28 +0900

    add new function to use this in program

--- README.md
+++ README.md
@@ -1,14 +1,42 @@
 # upvn
 ====
 
-This library is command line tool to update version info in the package.json with current version info in the NPM.  
+This library is tool to update version info in the package.json with current version info in the NPM.  
 Mainly this library is used to update npm version before publish.
 
 ## Purpose
 Update version info before publish library to NPM.
 
 ## Usage
+This library have two ways to use.  
+Command line mode and library mode.
+Command line mode is made for purpose of CI.  
+Library mode is made to update version data in program.
+
+
+### Command line
+In this mode, latest version is set from NPM information data.  
+And next, update package.json with updated version data.
+
 `$upvn [parameter]`  
+
+
+### In the Program
+
+```
+const upvn = require('upvn');
+
+const test = 'v0.1.2-beta4';
+
+upvn.update(test, 'major');     // -> v1.0.0
+upvn.update(test, 'minor');     // -> v0.2.0
+upvn.update(test, 'patch');     // -> v0.1.3
+upvn.update(test, 'v1.1.1');    // -> v1.1.1
+upvn.update(test, 'premajor');  // -> v1.0.0-beta0
+upvn.update(test, 'preminor');  // -> v0.2.0-beta0
+upvn.update(test, 'prepatch');  // -> v0.1.3-beta0
+upvn.update(test, 'prerelease');// -> v0.1.2-beta5
+```
 
 
 ### About parameter
--- index.js
+++ index.js
@@ -16,7 +16,94 @@ const ARGTYPE = {
 const PACKAGE = './package.json';
 
 const Upvn = {
-  update: (options) => {
+
+  checkFormat: (value, options) => {
+    let usable = false;
+    if (ARGTYPE[value]) {
+      usable = true;
+      options.type = value;
+      options.value = ARGTYPE[value];
+    } else if (value && value.length > 0 && value.indexOf('.') > 0) { // check comma
+      let cnt = 0;
+      for (let i = 1; i < value.length - 1; i++) {
+        if (value[i] === '.') {
+          cnt += 1;
+        }
+      }
+      if (cnt >= 2) {
+        usable = true;
+        options.newversion = value;
+      } else {
+        console.log('version format is incorrect.');
+        console.log('FORMAT: [major].[minor].[patch]-[release]');
+      }
+    } else {
+      console.log('unusable input. set as follows');
+      console.log('<newversion> | major | minor | patch '
+        + '| premajor | preminor | prepatch | prerelease'); 
+    }
+    return usable;
+  },
+
+  _updateVersion: (version, options) => {
+    if (!options) {
+      return null;
+    }
+
+    let newversion = options.newversion || null;
+    if (newversion) {
+      return newversion;
+    }
+    // check major, minor, patch and release info
+    const re = /(\D*)(\d+).(\d+).(\d+)(-*)(\D*)(\d*)/;
+    const array = version.match(re);
+    
+    const head  = array[1] || '';
+    let major = array[2] || 0;
+    let minor = array[3] || 0;
+    let patch = array[4] || 0;
+    const div = array[5] || '-';
+    const tag = array[6] || '';
+    let rel   = array[7] || 0;
+    
+    // update version
+    const datatype  = options.type;
+    const datavalue = options.value;
+  
+    if (datatype === 'major') {
+      major = parseInt(major, 10) + 1;
+      minor = 0;
+      patch = 0;
+    } else if (datatype === 'minor') {
+      minor = parseInt(minor, 10) + 1;
+      patch = 0;
+    } else if (datatype === 'patch') {
+      patch = parseInt(patch, 10) + 1;
+    } else if (datatype === 'premajor') {
+      major = parseInt(major, 10) + 1;
+      minor = 0;
+      patch = 0;
+      rel   = 0;
+    } else if (datatype === 'preminor') {
+      minor = parseInt(minor, 10) + 1;
+      patch = 0;
+      rel   = 0;
+    } else if (datatype === 'prepatch') {
+      patch = parseInt(patch, 10) + 1;
+      rel   = 0;
+    } else if (datatype === 'prerelease') {
+      rel   = parseInt(rel, 10) + 1;
+    }
+    newversion = '';
+    newversion = `${head}${major}.${minor}.${patch}`;
+    if (datavalue === 2) {
+      newversion += `${div}${tag}${rel}`;
+    }
+
+    return newversion;
+  },
+
+  updatePackage: (options) => {
     return new Promise((resolve, reject) => {
       Promise.resolve(options)
         .then((data) => {
@@ -44,58 +131,8 @@ const Upvn = {
           });
         })
         .then((data) => { // increment version
-          if (data.newversion) {
-            return data;
-          }
           const version = data.version;
-          // check major, minor, patch and release info
-          const re = /(\D*)(\d+).(\d+).(\d+)(-*)(\D*)(\d*)/;
-          const array = version.match(re);
-          
-          const head  = array[1] || '';
-          let major = array[2] || 0;
-          let minor = array[3] || 0;
-          let patch = array[4] || 0;
-          const div = array[5] || '-';
-          const tag = array[6] || '';
-          let rel   = array[7] || 0;
-          
-          // update version
-          const datatype  = data.type;
-          const datavalue = data.value;
-        
-          if (datatype === 'major') {
-            major = parseInt(major, 10) + 1;
-            minor = 0;
-            patch = 0;
-          } else if (datatype === 'minor') {
-            minor = parseInt(minor, 10) + 1;
-            patch = 0;
-          } else if (datatype === 'patch') {
-            patch = parseInt(patch, 10) + 1;
-          } else if (datatype === 'premajor') {
-            major = parseInt(major, 10) + 1;
-            minor = 0;
-            patch = 0;
-            rel   = 0;
-          } else if (datatype === 'preminor') {
-            minor = parseInt(minor, 10) + 1;
-            patch = 0;
-            rel   = 0;
-          } else if (datatype === 'prepatch') {
-            patch = parseInt(patch, 10) + 1;
-            rel   = 0;
-          } else if (datatype === 'prerelease') {
-            rel   = parseInt(rel, 10) + 1;
-          }
-          let newversion = '';
-          newversion = `${head}${major}.${minor}.${patch}`;
-          if (datavalue === 2) {
-            newversion += `${div}${tag}${rel}`;
-          }
-        
-          data.newversion = newversion;
-        
+          data.newversion = Upvn._updateVersion(version, data);
           return data;
         })
         .then((data) => { // update version in package.json
@@ -130,6 +167,19 @@ const Upvn = {
           reject(e);
         });
     });
+  },
+
+  update: (version, value) => {
+    // check format
+    const data = {};
+    let usable = false;
+    usable = Upvn.checkFormat(value, data);
+    if (usable === false) {
+      return null;
+    }
+    // update version
+    const newversion = Upvn._updateVersion(version, data);
+    return newversion;
   }
 };
 
